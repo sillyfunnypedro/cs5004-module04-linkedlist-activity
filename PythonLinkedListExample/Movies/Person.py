239defde224d6ee72be24147fe603fe4d0ccae67
Lino Coria Mendoza <l.coria@northeastern.edu>
2024-02-04 19:41:26 -0800

    - Added some getters - Changed some names to snake_case - Updated a unit test (it was naming Federico Fellini instead of Billy Wilder)

--- PythonLinkedListExample/Movies/Person.py
+++ PythonLinkedListExample/Movies/Person.py
@@ -1,38 +1,42 @@
-## class that represents a person
+# class that represents a person
 class Person:
     def __init__(self, first_name, last_name):
         self._first_name = first_name
         self._last_name = last_name
 
+    def get_first_name(self) -> str:
+        return self._first_name
+
+    def get_last_name(self) -> str:
+        return self._last_name
+
     def __str__(self):
         return self._first_name + " " + self._last_name
 
-
-# override the compare method
-
+    # override the compare method
 
     def __eq__(self, other):
         # check if the other object is a Person
         if not isinstance(other, Person):
             return False
-        if (self._first_name == other._first_name
-                and self._last_name == other._last_name):
+        if (self._first_name == other.get_first_name()
+                and self._last_name == other.get_last_name()):
             return True
         return False
 
     def __lt__(self, other):
-        if self._last_name < other._last_name:
+        if self._last_name < other.get_last_name():
             return True
-        elif self._last_name == other._last_name:
-            if self._first_name < other._first_name:
+        elif self._last_name == other.get_last_name():
+            if self._first_name < other.get_first_name():
                 return True
         return False
 
     def __gt__(self, other):
-        if self._last_name > other._last_name:
+        if self._last_name > other.get_last_name():
             return True
-        elif self._last_name == other._last_name:
-            if self._first_name > other._first_name:
+        elif self._last_name == other.get_last_name():
+            if self._first_name > other.get_first_name():
                 return True
         return False
 
@@ -41,17 +45,17 @@ class Person:
 # The unit tests for the Person class
 ##################################################################
 import unittest
+
+
 class TestPerson(unittest.TestCase):
 
     def setUp(self):
         pass
 
-
     def test_init(self):
         p = Person("John", "Doe")
         self.assertEqual("John", p._first_name)
         self.assertEqual("Doe", p._last_name)
-
 
     def test_str(self):
         p = Person("John", "Doe")
@@ -68,14 +72,14 @@ class TestPerson(unittest.TestCase):
         p1 = Person("John", "Doe")
         p2 = Person("Jane", "Doe")
         p3 = Person("John", "Smith")
-        self.assertTrue(p2 < p1) # Jane < John
-        self.assertTrue(p1 < p3) # Doe < Smith
-        self.assertTrue(p2 < p3) # Doe < Smith
+        self.assertTrue(p2 < p1)  # Jane < John
+        self.assertTrue(p1 < p3)  # Doe < Smith
+        self.assertTrue(p2 < p3)  # Doe < Smith
 
     def test_gt(self):
         p1 = Person("John", "Doe")
         p2 = Person("Jane", "Doe")
         p3 = Person("John", "Smith")
-        self.assertTrue(p1 > p2) # John > Jane
-        self.assertTrue(p3 > p1) # Smith > Doe
-        self.assertTrue(p3 > p2) # Smith > Doe
+        self.assertTrue(p1 > p2)  # John > Jane
+        self.assertTrue(p3 > p1)  # Smith > Doe
+        self.assertTrue(p3 > p2)  # Smith > Doe
